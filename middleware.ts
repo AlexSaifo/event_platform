d7c41336196bdc924ab0d3773ed3842f6cc11d99
Alex Saifo <saifoa808@gmail.com>
2024-04-18 02:47:30 +0300

    Fix Images  Routes

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default authMiddleware({
   ],
   ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"],
 });
-
+ 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)" , "/assets(.*)"],
 };
