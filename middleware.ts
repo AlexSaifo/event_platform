c59fffc3b3ecde584fa548961cbef2e219f3fc5a
Alex Saifo <saifoa808@gmail.com>
2024-04-18 02:38:33 +0300

    Fix Images  Routes

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({///assets/images/logo.svg
+export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    '/assets/images/logo.svg'
+    '/assets',
   ],
   ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)" , "/assets(.*)"],
 };
